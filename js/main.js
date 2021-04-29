a81c4ca0ba790978684613aaf9855f7bc8eb95de
mtunzisteven <mtunzism@byui.edu>
2021-04-29 04:15:26 +0200

    notes

--- js/main.js
+++ js/main.js
@@ -3,28 +3,44 @@ window.onload = function() {
     const links = [
                     {
                         label: 'Week1',
-                        url: 'week1/index.html'
+                        url: 'week1/'
                     },
                     {
                         label: 'Week2',
-                        url: 'week2/index.html'
+                        url: 'week2/'
                     }
                 
     ];
 
-    var ol = document.getElementById("olist");
+
+    append('olist', 'url', 'label');
+
+
+    const weel1 = [
+                    {
+                        label: 'notes',
+                        url: 'notes/'
+                    }    
+    ];
+
+    append('olist1', 'url', 'label');
+
+
+}
+
+function append(listId, url, label){
+    var ol = document.getElementById(listId);
 
     for(let i=0; i<links.length; i++){
 
         var li = document.createElement("LI");   
         var a = document.createElement("A");  
 
-        a.href =links[i]['url'];
-        a.innerHTML = links[i]['label'];
+        a.href =links[i][url];
+        a.innerHTML = links[i][label];
 
         li.appendChild(a);             
 
         ol.appendChild(li);  
     }
-
 }
