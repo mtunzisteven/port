2b4c39dc2ad488ea366cf1c593f9f7bce3864187
mtunzisteven <mtunzism@byui.edu>
2021-04-29 03:04:21 +0200

    main js fn

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function links(){
+    var liLinks='';
+    const links = [
+                    {
+                        label: 'Week1',
+                        url: 'week1/index.html'
+                    },
+                    {
+                        label: 'Week2',
+                        url: 'week2/index.html'
+                    },
+                
+    ]
+
+     for(let i=0; i< links.length; i++){
+        liLinks += "<li><a href='links[i][url]'>linksi[i][label]</a></li>";
+    }
+
+        document.getElementsById('olist').innerHTML = liLinks;
+}
